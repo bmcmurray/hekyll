42fcd5cb039005b7f9a22a489a96de6ffe52eabb
Brian McMurray <brian@brianmcmurray.com>
2012-01-24 21:26:31 -0500

    Experimenting with a new color scheme and SASS generated stylesheet, using Compass.

---
+++ _sass/config.rb
@@ -0,0 +1,30 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+# Paths to the base theme directories.
+css_dir = "../css"
+sass_dir = "../_sass"
+javascripts_dir = "../js"
+project_type = :stand_alone
+
+# Images are located in a shared library. Sprites are automatically generated.
+# images_dir = "docroot/sites/all/libraries/assets"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
