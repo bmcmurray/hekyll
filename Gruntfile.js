3c87bf63144e3aec877cb93e426acba2b34638c0
Peter Hillerström <peter.hillerstrom@gmail.com>
2015-09-15 12:40:07 +0300

    Include images on subdirectories on Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: '_img',
-                    src: ['*.{png,jpg,gif}'],
+                    src: ['**/*.{png,jpg,gif}'],
                     dest: 'img'
                 }]
             }
